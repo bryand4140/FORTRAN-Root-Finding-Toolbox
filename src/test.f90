d96924c4abb3a6085435b91e5909653943a90b32
Bryan Durham <69902245+bryand4140@users.noreply.github.com>
2025-02-04 16:32:09 -0700

    Added initial version of the user guide

--- src/test.f90
+++ src/test.f90
@@ -8,42 +8,27 @@ program test
     procedure(sv_function), pointer :: f_pointer => null()
     type(fun_zero_options) :: options
 
-    real(pv) :: coeff(3)
-    complex(pv) :: pzeros(2)
-    integer :: deg
-
-
-
     bounds = [-2.0, 2.0]
-    f_pointer => f
+    f_pointer => my_function  ! Pointer to the function to be solved
 
     options%show_iterations = .true.
     options%show_results    = .true.
     options%maxiter         = 100
     options%tolerance       = 1.0E-6_pv
 
-    !call fun_zero(f_pointer, bounds, status, root, f0_options = options)
-    coeff = [1.0, -1.0, -3.0]
-    deg   = 2
-    call poly_roots(coeff, deg, pzeros, status)
-    if(status == 0) then
-        print *, 'Roots: ', pzeros
-    else
-        print *, 'No roots found'
-    end if
+    call fun_zero(f_pointer, bounds, status, root, f0_options = options)
 
 
     contains
 
-    function f(x) result(y)
+    function my_function(x) result(y)
         real(pv), intent(in) :: x
         real(pv) :: y
         real(pv) :: pi
 
         pi = 3.141592653_pv
         y = -exp(-x) + 2.0 * x**2 / (exp(4.0*x)) - pi
-        !y = -log(1.0/x) + exp(-1.0 / x)
-    end function f
+    end function my_function
 
 
 end program test
